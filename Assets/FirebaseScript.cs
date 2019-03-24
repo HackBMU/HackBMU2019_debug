502467281603a0a75f780ecf68a818bbb790560f
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2019-03-24 07:53:28 +0530

    version 0.2

---
+++ Assets/FirebaseScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Auth;
+using Firebase;
+using UnityEngine.SceneManagement;
+using BayatGames.SaveGameFree;
+public class FirebaseScript : MonoBehaviour
+{
+	public InputField email, password, confirmpassword;
+	public InputField name, age;
+    public void loginbutton()
+	{
+		FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(email.text, password.text).ContinueWith((obj) =>
+		{
+			SceneManager.LoadSceneAsync(4);
+		});
+	}
+
+	public void signup_button()
+	{
+		SaveGame.Save<int>("age", int.Parse(age.text.ToString()));
+		SaveGame.Save<string>("name", name.ToString());
+		if (password.text == confirmpassword.text)
+		{
+			FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(email.text, password.text).ContinueWith((obj) =>
+			{
+				SceneManager.LoadSceneAsync(3);
+			});
+		}
+		else
+		{
+			Debug.Log("unsuccessful");
+		}
+	}
+}
