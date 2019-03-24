05347740df31464cb2f0a443ce57a428244f6676
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2019-03-24 07:50:01 +0530

    new version

--- Assets/scene_manage.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-public class scene_manage : MonoBehaviour
-{
-	public void gotoScene(int scene_number)
-	{
-		SceneManager.LoadSceneAsync(scene_number);
-	}
-}
